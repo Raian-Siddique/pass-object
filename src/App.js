782e58bbc878d65fe3bf9d69428bb2f6939d84d6
Raian-Siddique <raian0197@gmail.com>
2020-11-10 10:31:12 +0600

    passing object

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const products = [{ name: "Photoshop", price: "$90.99" }];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Product name={products[0].name}></Product>
+        <Product></Product>
+        <Product></Product>
       </header>
+    </div>
+  );
+}
+function Product(props) {
+  const productStyle = {
+    border: "1px solid light-salmon",
+    borderRadius: "5px",
+    backgroundColor: "lightgray",
+    height: "200px",
+    width: "200px",
+    float: "left",
+    margin: "5px",
+    padding: "10px",
+  };
+  return (
+    <div style={productStyle}>
+      <h2>{props.name}</h2>
+      <button>Buy Now</button>
     </div>
   );
 }
