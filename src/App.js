4fd4e75dabe450d3c39744ecb56d865854d6f856
Raian-Siddique <raian0197@gmail.com>
2020-11-12 09:09:58 +0600

    props passing to object

--- src/App.js
+++ src/App.js
@@ -2,23 +2,43 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const products = [
+  const nayokList = ["JASHIM", "RUBEL", "MANNA", "bappi"];
+  const pd = [
     { name: "Photoshop", price: "$90.99" },
     { name: "Illustrator", price: "$50.99" },
     { name: "Pdf reader", price: "$7.99" },
   ];
+  // const productNames = pd.map((pd) => pd.name);
+  // console.log(productNames);
+  // const nayokListNames = nayokList.map((nayokList) => nayokList);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Product product={products[0]}></Product>
-        <Product product={products[1]}></Product>
-        <Product product={products[2]}></Product>
+        <ul>
+          {pd.map((product) => (
+            <li>{product.name}</li>
+          ))}
+        </ul>
+        <ul>
+          {nayokList.map((nayok) => (
+            <li>{nayok}</li>
+          ))}
+          {/* <li>{nayokList[0]}</li>
+          <li>{nayokList[1]}</li> */}
+        </ul>
+        {pd.map((pds) => (
+          <Product product={pds}></Product>
+        ))}
+
+        {/* <Product product={pd[0]}></Product>
+        <Product product={pd[1]}></Product> */}
       </header>
     </div>
   );
 }
 function Product(props) {
-  const productStyle = {
+  const pdstyle = {
     border: "1px solid light-salmon",
     borderRadius: "5px",
     backgroundColor: "lightgray",
@@ -31,7 +51,7 @@ function Product(props) {
   const { name, price } = props.product;
 
   return (
-    <div style={productStyle}>
+    <div style={pdstyle}>
       <h2>{name}</h2>
       <h3>{price}</h3>
       <button>Buy Now</button>
